b99159f434a3564590b594a0dc943bc8dad8abcb
Lusnikov Vasily <lusnikoff.vasya@yandex.ru>
2020-10-21 23:31:58 +0700

    lab3

--- src/main/java/com/company/lusnikov/nsu/lab3/Lab3.java
+++ src/main/java/com/company/lusnikov/nsu/lab3/Lab3.java
@@ -1,27 +1,67 @@
 package com.company.lusnikov.nsu.lab3;
 
+
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lab3 {
+    public static List<String> names = new ArrayList<>();
     public static void main(String[] args) throws URISyntaxException, MalformedURLException, ClassNotFoundException {
-        File file = new File(Lab3.class.getResource("/A").toURI());
-        System.out.println(file);
-        //for (File f : file.listFiles()) {
-           // System.out.println(f);
-            URL url = file.toURI().toURL();          // file:/c:/myclasses/
-            URL[] urls = new URL[]{url};
-            ClassLoader cl = new URLClassLoader(urls);
-            System.out.println(cl);
-            //Class cls = cl.loadClass(f.getName().split("\\.")[0]);
 
-            Class cls = cl.loadClass("App");
-            System.out.println("cls = " + cls);
-          //  break;
-       // }
+        File file = new File("C:\\Users\\Vasya\\IdeaProjects\\javalabs\\src\\main\\resources\\A");
+        URL[] urls1 = new URL[]{file.toURI().toURL()};
+        ClassLoader cl = new URLClassLoader(urls1);
+        getClassNames(file, null);
+       for (String s : names) {
+            Class<?> aClass = cl.loadClass(s);
+            try {
+                Method method = aClass.getDeclaredMethod("getSecurityMessage");
+                Object object = aClass.getConstructor().newInstance();
+                method.setAccessible(true);
+                System.out.println(aClass.getName());
+                System.out.println(method.invoke(object));
 
+            } catch (NoSuchMethodException | InstantiationException | InvocationTargetException | IllegalAccessException e) {
+
+            }
+        }
+    }
+
+    public static void getClassNames(File dir, String parentName) {
+        for (File f : dir.listFiles()) {
+            if(!f.isDirectory()) {
+                StringBuilder stringBuilder = new StringBuilder();
+                List<String> list = Arrays.asList(f.getName().split("\\."));
+                for (int i = 0; i < list.size() - 1 ; i++) {
+                    stringBuilder.append(list.get(i));
+                    if (i != list.size() - 2) {
+                        stringBuilder.append(".");
+                    }
+                }
+                if (parentName == null) {
+                    names.add(stringBuilder.toString());
+                } else {
+                    names.add(parentName+ "."+ stringBuilder.toString());
+                }
+            }
+            else {
+                if (parentName == null) {
+                    getClassNames(f, f.getName());
+                } else {
+                    getClassNames(f, parentName + "." + f.getName());
+                }
+            }
+        }
     }
 }
